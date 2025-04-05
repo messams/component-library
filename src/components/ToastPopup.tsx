5f1155d2b845a8150b4c5ce49d8216e75d8a0fd3
m-essam-s <sir.m.essam.g@gmail.com>
2025-04-05 22:13:10 +0200

    Init: ToastPopup component

---
+++ src/components/ToastPopup.tsx
@@ -0,0 +1,11 @@
+const ToastPopup = () => {
+    return (
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`text-2xl font-medium`}>Toast Popups</h1>
+            </div>
+        </div>
+    );
+}
+
+export default ToastPopup;
