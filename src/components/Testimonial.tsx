320f3516bdff30bf51519a60c87bfb90d2af3668
m-essam-s <sir.m.essam.g@gmail.com>
2025-04-05 22:13:10 +0200

    Init: Testimonial component

---
+++ src/components/Testimonial.tsx
@@ -0,0 +1,11 @@
+const Testimonial = () => {
+    return (
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`text-2xl font-medium`}>Testimonials</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Testimonial;
