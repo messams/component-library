18941dd2a0e427077d12f8f7a94589e6a7418e73
m-essam-s <sir.m.essam.g@gmail.com>
2025-04-05 22:13:09 +0200

    Init: Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,11 @@
+const Card = () => {
+    return (
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`text-2xl font-medium`}>Cards</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
