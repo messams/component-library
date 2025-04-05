dbeccaa67f30741139b861f7e79a120c2cf9bfeb
m-essam-s <sir.m.essam.g@gmail.com>
2025-04-05 22:13:12 +0200

    Init: Tooltip component

---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,11 @@
+const Tooltip = () => {
+    return (
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`text-2xl font-medium`}>Tooltips</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Tooltip;
